da8595c4dc055169f96f6ea7974e8a5627a42808
Dominic LoBue <dominic.lobue@gmail.com>
2013-08-02 18:24:17 -0700

    and result after the reload is added

--- builders/builder.py
+++ builders/builder.py
@@ -3,7 +3,7 @@ from ._common_config import printy_thing, MYVAR
 
 
 def run():
-    print("this is the second builder version")
+    print("this is the third builder")
     printy_thing()
     print(MYVAR)
 
--- master.py
+++ master.py
@@ -1,13 +1,15 @@
 
 
 import imp
+import sys
 
 
 import builders
 
 
-print("loading builder v2")
+print("loading builder v3")
 
+imp.reload(sys.modules["builders._common_config"])
 
 builder = imp.load_source("builders.builder", "builders/builder.py")
 builder.run()
